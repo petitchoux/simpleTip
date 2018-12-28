81f8bb91f74b8e28314df61a3895d8ff08bd314f
Joyce <joycalation@gmail.com>
2018-12-27 21:55:08 -0500

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Joyce Huang on 12/27/18.
+//  Copyright © 2018 Joyce Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
