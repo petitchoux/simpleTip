ffebe6bccbf2bc26fc25f16eb1737824fc91a945
Joyce <joycalation@gmail.com>
2018-12-29 18:36:48 -0500

    Initial Commit

--- Tip/ViewController.swift
+++ Tip/ViewController.swift
@@ -7,14 +7,49 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipSlider: UISlider!
+    @IBOutlet weak var splitSlider: UISlider!
+    @IBOutlet weak var tipPercentLabel: UILabel!
+    @IBOutlet weak var splitLabel: UILabel!
+    @IBOutlet weak var eachLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        billField.becomeFirstResponder()
     }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 
+    @IBAction func calculateTip(sender: Any) {
+        let tipPercent = Double((round(tipSlider.value) / 100))
+        let split = Double(round((splitSlider.value)))
+        let bill = Double(billField.text!) ?? 0
+        
+        let tip = bill * tipPercent
+        let total = bill + tip
+        let each = total / split
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        eachLabel.text = String(format: "$%.2f", each)
+    }
+    
+    @IBAction func tipSliderValueChange(sender: Any) {
+        tipPercentLabel.text = String(format: "%02d%%", Int(round((tipSlider.value))))
+    }
+    
+    @IBAction func splitSliderChange(sender: Any) {
+        splitLabel.text = "\(Int(round(splitSlider.value)))"
+    }
+    
 }
-
